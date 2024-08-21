1214101e4aba56ebec68119bb9dc5750e86cbdc6
Jakub Michalik <michalik.jakub@gmail.com>
2024-08-21 10:45:22 +0200

    fix session transition timing

--- src/uds/uds_session.cpp
+++ src/uds/uds_session.cpp
@@ -12,6 +12,12 @@ UdsSession::UdsSession(const TimingConfig& timing)
 
 bool UdsSession::transition(SessionType new_session)
 {
+    // Can always go to default
+    if (new_session == SessionType::Default) {
+        reset();
+        return true;
+    }
+
     // Programming session can only be entered from default
     if (new_session == SessionType::Programming &&
         m_session != SessionType::Default) {
@@ -20,6 +26,7 @@ bool UdsSession::transition(SessionType new_session)
 
     m_session   = new_session;
     m_timed_out = false;
+    // S3 timer will be refreshed by keep_alive() called from server
     return true;
 }
 
@@ -48,7 +55,8 @@ void UdsSession::update(uint32_t tick_ms)
 void UdsSession::keep_alive(uint32_t tick_ms)
 {
     m_timed_out   = false;
-    m_s3_deadline = tick_ms + m_timing.s3_ms;
+    if (m_session != SessionType::Default)
+        m_s3_deadline = tick_ms + m_timing.s3_ms;
 }
 
 } // namespace uds
