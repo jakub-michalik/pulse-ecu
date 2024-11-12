8ff311102d1474c01bb33a224e8a001044d503b5
Jakub Michalik <michalik.jakub@gmail.com>
2024-11-12 20:15:44 +0100

    session improvements

--- include/uds/uds_session.hpp
+++ include/uds/uds_session.hpp
@@ -5,35 +5,42 @@
 
 namespace uds {
 
+using SessionTimeoutCallback = void (*)(void* ctx);
+
 class UdsSession {
 public:
     explicit UdsSession(const TimingConfig& timing = kDefaultTiming);
 
-    // Current active session
     SessionType current() const { return m_session; }
 
-    // Transition to new session - validates transition is allowed
     bool transition(SessionType new_session);
 
-    // Reset to default session
     void reset();
 
-    // Call periodically - handles S3 timeout
+    // Call periodically with current tick_ms
     void update(uint32_t tick_ms);
 
-    // Reset S3 timer (call on any tester communication)
+    // Reset S3 timer (call on any diagnostic communication)
     void keep_alive(uint32_t tick_ms);
 
-    // Returns true if session has timed out (S3 expired without TesterPresent)
     bool is_timed_out() const { return m_timed_out; }
 
     const TimingConfig& timing() const { return m_timing; }
+
+    // Register a callback for session timeout notification
+    void set_timeout_callback(SessionTimeoutCallback cb, void* ctx) {
+        m_timeout_cb  = cb;
+        m_timeout_ctx = ctx;
+    }
 
 private:
     SessionType  m_session;
     TimingConfig m_timing;
     uint32_t     m_s3_deadline;
     bool         m_timed_out;
+
+    SessionTimeoutCallback m_timeout_cb;
+    void*                  m_timeout_ctx;
 };
 
 } // namespace uds
--- src/uds/uds_session.cpp
+++ src/uds/uds_session.cpp
@@ -7,18 +7,18 @@ UdsSession::UdsSession(const TimingConfig& timing)
     , m_timing(timing)
     , m_s3_deadline(0)
     , m_timed_out(false)
+    , m_timeout_cb(nullptr)
+    , m_timeout_ctx(nullptr)
 {
 }
 
 bool UdsSession::transition(SessionType new_session)
 {
-    // Can always go to default
     if (new_session == SessionType::Default) {
         reset();
         return true;
     }
 
-    // Programming session can only be entered from default
     if (new_session == SessionType::Programming &&
         m_session != SessionType::Default) {
         return false;
@@ -26,7 +26,6 @@ bool UdsSession::transition(SessionType new_session)
 
     m_session   = new_session;
     m_timed_out = false;
-    // S3 timer will be refreshed by keep_alive() called from server
     return true;
 }
 
@@ -49,17 +48,16 @@ void UdsSession::update(uint32_t tick_ms)
         m_timed_out   = true;
         m_session     = SessionType::Default;
         m_s3_deadline = 0;
+
+        if (m_timeout_cb) m_timeout_cb(m_timeout_ctx);
     }
 }
 
 void UdsSession::keep_alive(uint32_t tick_ms)
 {
-    m_timed_out   = false;
+    m_timed_out = false;
     if (m_session != SessionType::Default)
         m_s3_deadline = tick_ms + m_timing.s3_ms;
 }
 
 } // namespace uds
-
-// Note: In default session, s3 timer is not started.
-// The S3 timer only runs when in non-default sessions.
